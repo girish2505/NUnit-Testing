728769e9a8594f0b83d3dc75d7d035a4d692cf88
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-02 22:24:26 +0530

    [Girish] Printing the min num of notes in vending machine

---
+++ NthUnitTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NthUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine.PrintingNotes();
+
+        }
+}
+}
